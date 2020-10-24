da5ac19c1ab0f370063048db2df66c739eacf7f1
tibiw <tibor.waxmann@gmail.com>
2020-10-24 17:43:09 +0300

    Initial Commit

---
+++ X-0/X_0App.swift
@@ -0,0 +1,17 @@
+//
+//  X_0App.swift
+//  X-0
+//
+//  Created by Tibor Waxmann on 24/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct X_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
